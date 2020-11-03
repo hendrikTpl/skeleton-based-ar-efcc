9bbf770d8ad43d2f125e100e53ad1998cdee04b7
ajisetyoko <aji.setyoko.second@gmail.com>
2020-11-03 22:15:51 +0800

    add flask-server

---
+++ serving-side/app.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import logging
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+@app.route("/")
+@cross_origin()
+def helloWorld():
+  return "Hello, cross-origin-world!"
+
+
+# API route
+@app.route('/api', methods=['POST'])
+def api():
+    """API function
+
+    All model-specific logic to be defined in the get_model_api()
+    function
+    """
+    input_data = request.form['data']
+    # app.logger.info("api_input: " + str(input_data))
+    # output_data = model_api(input_data)
+    # app.logger.info("api_output: " + str(output_data))
+    print(input_data)
+    response = jsonify({
+        'Response': 'OK'
+    })
+    return response
+
+
+# @app.route('/')
+# def index():
+#     return "Succes API"
+
+
+@app.route('/communicate', methods=['POST', 'GET'])
+def communicate():
+    if request.method =='POST':
+        input_data = request.form['data']
+        print(input_data)
+
+        print(request.files)
+        if 'file' not in request.files:
+            return jsonify({
+                'detail':'JSON file not send correctly',
+                'return_value': str(0),
+            })
+
+        
+
+        #TODO: 1. Save data every request received in server.
+        #TODO: 2. Autmatic Sliding Window by total received data
+        #TODO: 3. JSON -> NPY; using data generator from ST-GCN DATAGEN
+        #TODO: 4. Action Prediction using ST-GCN-PAM
+        #TODO: 5. .......
+            
+        return jsonify({
+            'results':'File Received'
+        })
+
+import json
+@app.route('/posenet', methods=['POST', 'GET'])
+def posenet():
+    data = request.json
+    
+    print(data)
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+            
+    return jsonify({
+        'results':'File Received'
+    })
+
+def predict_action():
+    pass
+
+
+
+if __name__ == '__main__':
+    # This is used when running locally.
+    app.run(host='0.0.0.0', debug=True)
